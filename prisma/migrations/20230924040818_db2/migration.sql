d03845adbf87a1194ca8bfe78c421efcf6d5651e
osaket <o.alsaket@uqconnect.edu.au>
2023-09-25 11:59:56 +1000

    first commit - main files

---
+++ prisma/migrations/20230924040818_db2/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `ArticleRank` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ImageRank` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ModelRank` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ModelVersionRank` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `PostRank` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `TagRank` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `UserRank` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "ArticleRank";
+
+-- DropTable
+DROP TABLE "ImageRank";
+
+-- DropTable
+DROP TABLE "ModelRank";
+
+-- DropTable
+DROP TABLE "ModelVersionRank";
+
+-- DropTable
+DROP TABLE "PostRank";
+
+-- DropTable
+DROP TABLE "TagRank";
+
+-- DropTable
+DROP TABLE "UserRank";
